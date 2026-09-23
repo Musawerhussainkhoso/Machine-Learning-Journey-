56768c48b409f32b5dffc8235ed7078d1e115949
Musawer Hussain <musawerhussain10@gmail.com>
2026-09-23 08:49:49 -0700

    Refactor EDA script for improved data inspection and visualization

--- bank+marketing/bank/EDA.py
+++ bank+marketing/bank/EDA.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv(r"D:\ML journey 2027\bank+marketing\bank\bank.csv")
-
-print(df.head())
-print(df.shape)
-print(df.info())
-print(df.describe())
+#Step 2: Initial Inspection
+df.shape           # rows, columns
+df.head(10)        # first 10 rows
+df.tail()           
+df.columns
+df.dtypes           # data types of each column
+df.info()           # nulls + dtypes + memory usage
+df.describe()       # stats for numeric columns
+df.describe(include='object')  # stats for categorical columns
